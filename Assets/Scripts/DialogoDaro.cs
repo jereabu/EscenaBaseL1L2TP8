bde7694a20e6d8fd0cf8799a9b019e97a6b4ac00
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-09-08 09:00:31 -0300

    Started

---
+++ Assets/Scripts/DialogoDaro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogoDaro : MonoBehaviour
+    
+{
+    public GameObject Daro;
+    public GameObject dialogo;
+    public Button siguiente;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //al entrar activa la UI de diálogo
+        if (other.gameObject.CompareTag("NPC"))
+        {
+            dialogo.SetActive(true);
+        }
+    }
+    void OnTriggerExit()
+    {
+        dialogo.SetActive(false);
+    }
+
+}
